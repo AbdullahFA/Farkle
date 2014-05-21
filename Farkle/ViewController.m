6dce78337a138d0cc8cc24142550043e1841df80
Dennis Dixon <ddixonjr@appivot.com>
2014-05-21 18:03:46 -0500

    Updated comments to list original contributors

--- Farkle/ViewController.m
+++ Farkle/ViewController.m
@@ -2,7 +2,7 @@
 //  ViewController.m
 //  Farkle
 //
-//  Created by Dennis Dixon on 5/21/14.
+//  Started and Created by Dennis Dixon and Abdullah AlMubarak on 5/21/14.
 //  Copyright (c) 2014 Mobile Makers. All rights reserved.
 //
 
